f8d85e82fcae85bc26159b3b33eb0b604a723cb8
Lal007 <42681868+Lal007@users.noreply.github.com>
2019-07-24 23:53:03 +0300

    dz5 ready

--- src/main/Lesson_5/Car.java
+++ src/main/Lesson_5/Car.java
@@ -1,5 +1,7 @@
 package Lesson_5;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Car implements Runnable{
     private static int CARS_COUNT;
     static {
@@ -7,6 +9,8 @@ public class Car implements Runnable{
     }
     private Race race;
     private int speed;
+    private CountDownLatch cdl;
+    private CountDownLatch cd2;
     private String name;
     public String getName() {
         return name;
@@ -14,9 +18,11 @@ public class Car implements Runnable{
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CountDownLatch cdl, CountDownLatch cd2) {
         this.race = race;
         this.speed = speed;
+        this.cdl = cdl;
+        this.cd2 = cd2;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
@@ -26,11 +32,26 @@ public class Car implements Runnable{
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cdl.countDown();
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            cdl.await();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
+        if (cd2.getCount() == CARS_COUNT){
+            System.out.println(this.name + " WIN!");
+        }
+        cd2.countDown();
+        try {
+            cd2.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/Lesson_5/MainClass.java
+++ src/main/Lesson_5/MainClass.java
@@ -1,18 +1,35 @@
 package Lesson_5;
 
+import java.util.concurrent.CountDownLatch;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
+        CountDownLatch cd2 = new CountDownLatch(CARS_COUNT);
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cdl, cd2);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        while (true){
+            if (cdl.getCount() == 0){
+                System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+                break;
+            }
+        }
+
+        while (true){
+            if (cd2.getCount() == 0){
+                System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+                break;
+            }
+        }
+
     }
 }
