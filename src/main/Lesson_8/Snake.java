f1351dbfcfbde4c3a5b8c41ce3232e998e3e8d54
Lal007 <42681868+Lal007@users.noreply.github.com>
2019-08-03 12:48:56 +0300

    dz8 add snake

---
+++ src/main/Lesson_8/Snake.java
@@ -0,0 +1,40 @@
+package Lesson_8;
+
+public class Snake {
+
+    static final int SIZE = 7;
+    static int[][] matrix = new int[SIZE][SIZE];
+
+    public static void main(String[] args) {
+        int counter = 1;
+        int j, k, p = SIZE/2;
+
+        for (k = 1; k <= p; k++) {
+            for (j = k - 1; j < SIZE - k + 1; j++) {
+                matrix[k - 1][j] = counter++;
+            }
+            for (j = k; j < SIZE - k + 1; j++) {
+                matrix[j][SIZE - k] = counter++;
+            }
+            for (j = SIZE - k -1;j >= k -1; --j) {
+                matrix[SIZE - k][j] = counter++;
+            }
+            for (j = SIZE - k -1;j >= k; j--){
+                matrix[j][k - 1] = counter++;
+            }
+        }
+        if (SIZE % 2 != 0){
+            matrix[p][p] = SIZE * SIZE;
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int l = 0; l < SIZE; l++) {
+                int tmp = matrix[i][l];
+                if (tmp < 10){
+                    System.out.print(" " + tmp + " ");
+                }else System.out.print(tmp + " ");
+            }
+            System.out.println();
+        }
+    }
+}
